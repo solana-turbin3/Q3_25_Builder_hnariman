e0efa162897d15e739f5f50276e0a52d17582c32
Nariman Huseynov <hnariman@gmail.com>
2025-07-04 01:45:09 +0400

    feat: nft's
    lesson learned:
    - convert images
    - upload to decentralized storage
    - create metadata & upload to storage
    - mint NFT's

--- solana-starter/ts/cluster1/nft_metadata.ts
+++ solana-starter/ts/cluster1/nft_metadata.ts
@@ -1,7 +1,8 @@
-import wallet from "../turbin3-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import wallet from "/home/hnariman/turbin3-wallet.json";// with {type: "json"};
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import fs, { ReadStream } from 'fs';
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -9,6 +10,7 @@ const umi = createUmi('https://api.devnet.solana.com');
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
+// umi.use(irysUploader({ address: "https://devnet.irys.xyz/", }));
 umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
@@ -16,30 +18,39 @@ umi.use(signerIdentity(signer));
     try {
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+        // const p = fs.readFileSync("/home/hnariman/Pictures/1.jpg");
+        // const f = createGenericFile(p,"1.jpg");
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
+        // const imagePath = "https://gateway.irys.xyz/E6cGu23XZmfJ5nbdrPnwjcrM5yvehVcbQsGboZDudqQq"
+        // const image = "https://devnet.irys.xyz/EsXXhuYi7RajYksKY8M2cgrhj9x2iYMPsT2tCdfq16Da";
+        //
+        const imagePath = "https://gateway.irys.xyz/AwsCLUK3B9zs5rN5eN9BScLx7CgRu3tLP5yygDyQepsP"
+        const metadata = {
+            name: "Turbin3 Duck One",
+            symbol: "TRD1",
+            description: "Ducks Everywhere",
+            image: imagePath,
+            attributes: [
+                { trait_type: 'generated', value: 'with excitement!' }
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: imagePath,
+                    },
+                ],
+                category: "image"
+            },
+            creators: [{
+                addres: signer.publicKey.toString(),
+                share: 100
+            }]
+        };
+        const myUri = await umi.uploader.uploadJson(metadata);
+        console.log("Your metadata URI: ", myUri);
     }
-    catch(error) {
+    catch (error) {
         console.log("Oops.. Something went wrong", error);
     }
 })();
--- solana-starter/ts/cluster1/nft_mint.ts
+++ solana-starter/ts/cluster1/nft_mint.ts
@@ -2,25 +2,38 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../turbin3-wallet.json"
+import wallet from "/home/hnariman/turbin3-wallet.json";// with {type: "json"};
 import base58 from "bs58";
+import { trace } from "console";
 
-const RPC_ENDPOINT = "https://api.devnet.solana.com";
+// const metadataURI = "https://gateway.irys.xyz/2iWw37ZfzCB1wgYHgHFyuH7xjhXA3hkyVzbr99r2TZYT";
+const metadataURI = "https://gateway.irys.xyz/HGCZB6EovMtH6auXpV6hd7KmVBu92fNTSAjXS9geSY4z"
+
+// const RPC_ENDPOINT = "https://api.devnet.solana.com";
+const RPC_ENDPOINT = "https://devnet.helius-rpc.com/?api-key=71d05d9f-5d94-4548-9137-c6c3d9f69b3e"
 const umi = createUmi(RPC_ENDPOINT);
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const myKeypairSigner = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(myKeypairSigner));
-umi.use(mplTokenMetadata())
+umi.use(mplTokenMetadata());
 
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    let tx = createNft(umi, {
+        mint,//: mint.publicKey,
+        name: "Tubin3 Duck One",
+        symbol: "TRD1",
+        uri: metadataURI,
+        sellerFeeBasisPoints: percentAmount(1),
+        isMutable: true,
+        collectionDetails: null
+    });
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
+
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
 })();
